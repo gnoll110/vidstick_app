a47358f7192270276a5bcb33f87718c9649389f6
Noel Kelly <noel@noelkelly.biz>
2015-06-23 23:58:29 +1000

    Dup path spec, for Source model

--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Source, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
+  context "test vilidate" do
+    it "dup path" do
+      source = Source.create
+      source.path = "/home/noel/Downloads"
+      source.save
+      source = Source.create
+      source.path = "/home/noel/Downloads"
+      source.save
+    end
+  end
 end
