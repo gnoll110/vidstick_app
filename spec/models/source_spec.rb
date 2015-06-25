7126eff0492602ad62a100dcb5e1a07254d30907
Noel Kelly <noel@noelkelly.biz>
2015-06-25 18:37:33 +1000

    add path too short test to source model spec

--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe Source, type: :model do
       expect(Source.count).to eq(1)
       expect(source.errors.full_messages).to eq(['Path has already been taken'])
     end
+
+    it "too short path" do
+      source = Source.create
+      source.path = "d"
+      source.save
+      expect(Source.count).to eq(0)
+      expect(source.errors.full_messages).to eq(['Path is too short (minimum is 2 characters)'])
+    end
   end
 end
