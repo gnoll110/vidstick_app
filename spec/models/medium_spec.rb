93a0145b66f806a156c847177112c6f0402e8332
Noel Kelly <noel@noelkelly.biz>
2015-06-25 19:49:55 +1000

    add filename too short test to medium model spec

--- spec/models/medium_spec.rb
+++ spec/models/medium_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe Medium, type: :model do
       expect(Medium.count).to eq(1)
       expect(medium.errors.full_messages).to eq(['Filename has already been taken'])
     end
+
+    it "filename too short" do
+      medium = Medium.create
+      medium.filename = "d"
+      medium.save
+      expect(Medium.count).to eq(0)
+      expect(medium.errors.full_messages).to eq(['Filename is too short (minimum is 2 characters)'])
+    end
   end
 end
