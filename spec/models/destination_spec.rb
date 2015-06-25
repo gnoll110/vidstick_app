de0648a68d8f7d2b2c9f0722ef466914fda844fa
Noel Kelly <noel@noelkelly.biz>
2015-06-25 18:08:15 +1000

    add dup path test to destination model spec

--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Destination, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
+  context "test vilidate" do
+    it "dup path" do
+      dest = Destination.create
+      dest.path = "dev/ruby"
+      dest.save
+      dest = Destination.create
+      dest.path = "dev/ruby"
+      dest.save
+      expect(Destination.count).to eq(1)
+      expect(dest.errors.full_messages).to eq(['Path has already been taken'])
+    end
+  end
 end
