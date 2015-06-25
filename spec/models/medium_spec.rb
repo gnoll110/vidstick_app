7d32f04843b9bfc1e0d6878c03ad614036baa404
Noel Kelly <noel@noelkelly.biz>
2015-06-25 18:56:56 +1000

    add dup filename to medium model spec

--- spec/models/medium_spec.rb
+++ spec/models/medium_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Medium, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
+  context "test vilidate" do
+    it "dup path" do
+      medium = Medium.create
+      medium.filename = "rails.mp4"
+      medium.save
+      medium = Medium.create
+      medium.filename = "rails.mp4"
+      medium.save
+      expect(Medium.count).to eq(1)
+      expect(medium.errors.full_messages).to eq(['Filename has already been taken'])
+    end
+  end
 end
