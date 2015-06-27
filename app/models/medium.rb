fbab9a875981662d3e28d45d0a1aaf2f66c14c2e
Noel Kelly <noel@noelkelly.biz>
2015-06-27 23:48:00 +1000

    modify media destination many-to-many

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,9 +1,10 @@
 class Medium < ActiveRecord::Base
   has_many :relations
+  has_many :destinations, :through => :relations
 
-  accepts_nested_attributes_for :relations,
-                                reject_if: proc { |attributes| attributes['path'].blank? },
-                                allow_destroy: true
+  #accepts_nested_attributes_for :relations,
+  #                              reject_if: proc { |attributes| attributes['path'].blank? },
+  #                              allow_destroy: true
 
   validates :filename, presence: true, uniqueness: true, length: { minimum: 2 }
   #validates :modified_filemale, uniqueness: true, length: { minimum: 2 }
