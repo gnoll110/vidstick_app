0a307a80ef5d010601424d262ff21d57faa1615d
Noel Kelly <noel@noelkelly.biz>
2015-07-07 23:24:12 +1000

    add stub unit spec: scan_sources and build_stick

---
+++ spec/unit/build_stick_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe BuildStick do
+  context ".process" do
+    it "basic build" do
+    end
+  end
+end
---
+++ spec/unit/scan_sources_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe ScanSources do
+  context ".process" do
+    it "basic scan" do
+    end
+  end
+end
