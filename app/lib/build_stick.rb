2f62364a307c60cdcab3badcf645b8939cd9d1a7
Noel Kelly <noel@noelkelly.biz>
2015-07-28 17:16:53 +1000

    start stubbing out methods in app/lib/build_stick.rb

--- app/lib/build_stick.rb
+++ app/lib/build_stick.rb
@@ -1,4 +1,53 @@
 class BuildStick
   def process
+    get_file_list
+    build_stick
+  end
+
+  def builder
+    form_data = params[:stick]
+    dest_array = form_data[:destination]
+    final_dest_array = []
+    dest_array.each do | dest |
+      if dest.length > 0
+        puts dest.to_i
+        final_dest_array << dest
+      end
+    end
+    puts final_dest_array
+    final_file_array = get_file_list(final_dest_array)
+    build_stick(final_file_array)
+    render 'index'    
+  end
+
+  def get_file_list(dest_array)
+  end
+
+  def build_stick(file_array)
+  end
+
+  def sump  
+    sources = Source.all
+    destination = Destination.find_by_path('eco/permaculture')
+    puts destination
+    #destinations.each do | destination |
+      puts destination.path
+      destination.media.each do | medium |
+        puts medium.filename
+        puts medium.id
+        sources.each do | source |
+          puts source.path
+          full_filename = File.join(source.path, medium.filename)
+          puts full_filename
+          if File.exists?(full_filename)
+            puts '>>>'+full_filename
+          end
+        end
+        medium.destinations.each do | final_dest |
+          puts '>>>>>>'+final_dest.path
+        end
+      end
+    #end
+    render 'index'    
   end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe WelcomeController, type: :controller do
     end
   end
 
+  describe "GET #loader" do
+    it "returns http success" do
+      get :loader
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #builder" do
+    it "returns http success" do
+      post :builder
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
