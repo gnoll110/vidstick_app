608b62c94b34e731c525c35bec794313e6aded3a
Noel Kelly <noel@noelkelly.biz>
2015-08-17 01:46:44 +1000

    add 'mp3' files for ScanSources

--- app/lib/scan_sources.rb
+++ app/lib/scan_sources.rb
@@ -3,14 +3,16 @@ class ScanSources
     sources = Source.all
     sources.each do | source |
       puts source.path
-      pattern = File.join(source.path, '*.mp4')
-      list = Dir.glob(pattern)
-      list.each do | filename |
-        puts filename
-        base = File.basename(filename)
-        medium = Medium.new
-        medium.filename = base
-        medium.save
+      ['mp4','mp3'].each do | ext |
+        pattern = File.join(source.path, '*.'+ext)
+        list = Dir.glob(pattern)
+        list.each do | filename |
+          puts filename
+          base = File.basename(filename)
+          medium = Medium.new
+          medium.filename = base
+          medium.save
+        end
       end
     end
   end
