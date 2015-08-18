f1f19799fad99ff4e08a130e0ca30766748a8d54
Noel Kelly <noel@noelkelly.biz>
2015-08-18 22:24:38 +1000

    clean up ScanSources puts

--- app/lib/scan_sources.rb
+++ app/lib/scan_sources.rb
@@ -2,12 +2,10 @@ class ScanSources
   def process
     sources = Source.all
     sources.each do | source |
-      puts source.path
       ['mp4','mp3'].each do | ext |
         pattern = File.join(source.path, '*.'+ext)
         list = Dir.glob(pattern)
         list.each do | filename |
-          puts filename
           base = File.basename(filename)
           medium = Medium.new
           medium.filename = base
