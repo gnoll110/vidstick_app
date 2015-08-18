7c37b9f757ae566a9f0bd8834427754f741a4ffe
Noel Kelly <noel@noelkelly.biz>
2015-08-18 22:48:20 +1000

    refactor ScanSources process method, several extract methods

--- app/lib/scan_sources.rb
+++ app/lib/scan_sources.rb
@@ -2,16 +2,30 @@ class ScanSources
   def process
     sources = Source.all
     sources.each do | source |
-      ['mp4','mp3'].each do | ext |
-        pattern = File.join(source.path, '*.'+ext)
-        list = Dir.glob(pattern)
-        list.each do | filename |
-          base = File.basename(filename)
-          medium = Medium.new
-          medium.filename = base
-          medium.save
-        end
-      end
+      proces_source
     end
+  end
+
+  private
+
+  def process_source
+    ['mp4','mp3'].each do | ext |
+      process_extention
+    end
+  end
+
+  def process_extention
+    pattern = File.join(source.path, '*.'+ext)
+    list = Dir.glob(pattern)
+    list.each do | filename |
+      process_file
+    end
+  end
+
+  def process_file
+    base = File.basename(filename)
+    medium = Medium.new
+    medium.filename = base
+    medium.save
   end
 end
