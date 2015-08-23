6bdd145d4c73283939c48fcbc38b80f367001ac1
Noel Kelly <noel@noelkelly.biz>
2015-08-23 22:00:43 +1000

    fixed rubocop messages in models, inc refactor 'proc' to method in medium

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,6 +1,6 @@
 class Destination < ActiveRecord::Base
   has_many :relations
-  has_many :media, :through => :relations
+  has_many :media, through: :relations
 
   validates :path, presence: true, uniqueness: true, length: { minimum: 2 }
 end
--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,11 +1,13 @@
 class Medium < ActiveRecord::Base
   has_many :relations
-  has_many :destinations, :through => :relations
+  has_many :destinations, through: :relations
 
   accepts_nested_attributes_for :relations,
-                                reject_if: proc { |attributes| attributes['destination_id'].blank? },
+                                reject_if: :reject_relations,
                                 allow_destroy: true
+  def reject_relations(attributes)
+    attributes['destination_id'].blank?
+  end
 
   validates :filename, presence: true, uniqueness: true, length: { minimum: 2 }
-  #validates :modified_filemale, uniqueness: true, length: { minimum: 2 }
 end
