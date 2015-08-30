16c27962510a862cec71ce96a33323cd8fe33c3d
Noel Kelly <noel@noelkelly.biz>
2015-08-30 23:01:06 +1000

    test refactor of app/lib/scan_sources.rb

--- app/lib/scan_sources.rb
+++ app/lib/scan_sources.rb
@@ -1,21 +1,25 @@
 class ScanSources
   def process
-    sources = Source.all
+    sources = Source.all 
     sources.each do |source|
-      proces_source(source)
+      puts source.path.to_s
+      process_source(source.path)
     end
   end
 
   private
 
   def process_source(source)
-    %w('mp4' 'mp3').each do |ext|
+  	puts "a"+source.to_s
+    %w(mp4 mp3).each do |ext|
       process_extention(source, ext)
     end
   end
 
   def process_extention(source, ext)
-    pattern = File.join(source.path, '*.' + ext)
+  	puts "b"+source.to_s+ext.to_s
+    pattern = File.join(source, '*.' + ext)
+    puts pattern
     list = Dir.glob(pattern)
     list.each do |filename|
       process_file(filename)
@@ -23,6 +27,7 @@ class ScanSources
   end
 
   def process_file(filename)
+  	puts "c"+filename.to_s
     base = File.basename(filename)
     medium = Medium.new
     medium.filename = base
