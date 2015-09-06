a3fba5d4bfc6dbd13d6b5227132de9f41f822112
Noel Kelly <noel@noelkelly.biz>
2015-09-06 21:47:22 +1000

    clean up puts in app/lib/scan_sources.rb

--- app/lib/scan_sources.rb
+++ app/lib/scan_sources.rb
@@ -2,7 +2,6 @@ class ScanSources
   def process
     sources = Source.all 
     sources.each do |source|
-      puts source.path.to_s
       process_source(source.path)
     end
   end
@@ -10,16 +9,13 @@ class ScanSources
   private
 
   def process_source(source)
-  	puts "a"+source.to_s
     %w(mp4 mp3).each do |ext|
       process_extention(source, ext)
     end
   end
 
   def process_extention(source, ext)
-  	puts "b"+source.to_s+ext.to_s
     pattern = File.join(source, '*.' + ext)
-    puts pattern
     list = Dir.glob(pattern)
     list.each do |filename|
       process_file(filename)
@@ -27,7 +23,6 @@ class ScanSources
   end
 
   def process_file(filename)
-  	puts "c"+filename.to_s
     base = File.basename(filename)
     medium = Medium.new
     medium.filename = base
