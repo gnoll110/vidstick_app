f448ef3946b4bc6fdd35a323aa8617c973cb8c53
Noel Kelly <noel@noelkelly.biz>
2015-09-06 22:24:10 +1000

    add all the factory_girl data into app/lib/build_stick.rb before :each

--- spec/unit/build_stick_spec.rb
+++ spec/unit/build_stick_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 describe BuildStick do
   before :each do
+    create(:source, :dwhelper)
+    create(:source, :downloads)
     create(:destination, :dev)
     create(:destination, :eco)
     create(:destination, :dev_rails)
+    create(:medium, :patterns)
+    create(:medium, :permaculture)
+    create(:medium, :hexagonal)
+    create(:relation, :dev_patterns)
+    create(:relation, :eco_permaculture)
+    create(:relation, :dev_rails_hexagonal)
   end
 
   context ".process" do
