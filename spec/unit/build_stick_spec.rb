b56714e2336bd13a67c3c3764e54f8fb52934329
Noel Kelly <noel@noelkelly.biz>
2015-09-12 23:45:54 +1000

    fix build_stick_spec.rb as initialize uses hash of params, so can inject DefaultSelector

--- spec/unit/build_stick_spec.rb
+++ spec/unit/build_stick_spec.rb
@@ -18,7 +18,7 @@ describe BuildStick do
   context ".process" do
     it "default build" do
       selector = DefaultSelector.new
-      builder = BuildStick.new(selector, {"destination"=>["", "4", "20", "5"]})
+      builder = BuildStick.new({ selector: selector, stick: {"destination"=>["", "4", "20", "5"]} })
       builder.process
     end
   end
