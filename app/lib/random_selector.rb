ca496ebb15d090f46e92183933c08d91db570f01
Noel Kelly <noel@noelkelly.biz>
2015-09-19 23:36:12 +1000

    Stub out run in RandonSelector & UnclassifirdSelector to return an empty array

--- app/lib/random_selector.rb
+++ app/lib/random_selector.rb
@@ -1,4 +1,11 @@
 class RandomSelector
   def run
+    final_dest_array = Array.new
+    #dest_array.each do |dest|
+    #  if dest.length > 0
+    #    final_dest_array << dest
+    #  end
+    #end
+    final_dest_array  	
   end
 end
--- app/lib/unclassified_selector.rb
+++ app/lib/unclassified_selector.rb
@@ -1,4 +1,11 @@
 class UnclassifiedSelector
   def run
+    final_dest_array = Array.new
+    #dest_array.each do |dest|
+    #  if dest.length > 0
+    #    final_dest_array << dest
+    #  end
+    #end
+    final_dest_array
   end
 end
