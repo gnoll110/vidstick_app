ff627aabeeeff33dbb649a0ce4380a876316a5c1
Noel Kelly <noel@noelkelly.biz>
2015-10-10 23:54:44 +1000

    remove random_selector & unclassified_selector

--- app/lib/random_selector.rb
+++
@@ -1,8 +0,0 @@
-class RandomSelector
-  def run
-    final_dest_array = Array.new
-    # select all media with > 0 relation records
-    # randon selection
-    final_dest_array  	
-  end
-end
--- app/lib/unclassified_selector.rb
+++
@@ -1,8 +0,0 @@
-class UnclassifiedSelector
-  def run
-    final_dest_array = Array.new
-    # select all media with no relation records
-    # randon selection
-    final_dest_array
-  end
-end
