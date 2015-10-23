5c71ec377d102270f962fa0c3c838dcdc7ee98c9
Noel Kelly <noel@noelkelly.biz>
2015-10-23 23:42:59 +1000

    add random_selector & unclassified_selector contexts to welcom_controler_spec

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -9,25 +9,40 @@ RSpec.describe WelcomeController, type: :controller do
     end
   end
 
-  describe "GET #loader" do
-    it "returns http success" do
-      get :loader
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "POST #builder" do
+  describe do
     before :each do
       create(:destination, :dev)
-      create(:destination, :eco)
+      create(:destination, :eco) 
       create(:destination, :dev_rails)
     end
 
-    it "returns http success" do
-      selector = DefaultSelector.new({"destination"=>["", "4", "20", "5"]})
-      post :builder, stick: {"destination"=>["", "4", "20", "5"]}
-      expect(response).to have_http_status(:success)
+    context "GET #loader" do
+      it "returns http success" do
+        get :loader
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "GET #random_builder" do
+      it "returns http success" do
+        get :random_builder
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "GET #unclassified_builder" do
+      it "returns http success" do
+        get :unclassified_builder
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "POST #builder" do
+      it "returns http success" do
+        selector = DefaultSelector.new({"destination"=>["", "4", "20", "5"]})
+        post :builder, stick: {"destination"=>["", "4", "20", "5"]}
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
 end
